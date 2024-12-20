287536b45777e1b5c5d5fc40d9c040067542be2d
MehdiHossenFahim <mehdihossenfahim100@gmail.com>
2024-12-20 14:44:24 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.querySelector('.cta-btn').addEventListener('click', () => {
+  alert('Learn more about Seesaw!');
+});
+
+document.querySelector('.login-btn').addEventListener('click', () => {
+  alert('Redirecting to login page...');
+});
